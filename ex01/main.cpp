aa5ab1580c4b3038a4772f8d7e4997458bc69797
kimia <kimiaziari1997@gmail.com>
2025-03-24 18:08:29 +0100

    fix the name of program

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,19 +1,12 @@
 #include "ScavTrap.hpp"
 
-int main() {
-	// Create a ScavTrap object
+int main()
+{
 	ScavTrap scavTrap("Guardian");
-
-	// Test attack function (inherited from ClapTrap)
+	
 	scavTrap.attack("Enemy");
-
-	// Test takeDamage function (inherited from ClapTrap)
 	scavTrap.takeDamage(20);
-
-	// Test beRepaired function (inherited from ClapTrap)
 	scavTrap.beRepaired(10);
-
-	// Test guardGate function (unique to ScavTrap)
 	scavTrap.guardGate();
 
 	return 0;
